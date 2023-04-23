f55818b9e5b1a1572010af4ce8170c878538f719
Dzmitry Salivon <saleevong@gmail.com>
2023-04-23 21:00:26 +0200

    refactor: removed unused code

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-//Document is the DOM can be accessed in the console with document.window.
-// Tree is from the top, html, body, p etc.
-
-//Problem: User interaction does not provide the correct results.
-//Solution: Add interactivity so the user can manage daily tasks.
-//Break things down into smaller steps and take each step at a time.
-
-// Event handling, user interaction is what starts the code execution.
-
 const taskLabel = document.querySelector(".task__label");
 
 var taskInput = document.getElementById("new-task"); //Add a new task.
@@ -200,9 +191,3 @@ for (var i = 0; i < completedTasksHolder.children.length; i++) {
   //bind events to list items chldren(tasksIncompleted)
   bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
 }
-
-// Issues with usability don't get seen until they are in front of a human tester.
-
-//prevent creation of empty tasks.
-
-//Change edit to save when you are in edit mode.
